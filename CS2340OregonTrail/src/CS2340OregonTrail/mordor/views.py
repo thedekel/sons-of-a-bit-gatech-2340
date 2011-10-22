118321489d9d0671974dfffcd17bb59f6ee38467
CaptainAndred <danieljwhatley@gmail.com>
2011-10-22 15:13:34 -0400

    Uploaded /res/styles.css. Also, I didn't change views.py, but it thinks I did. So I'm committing it so it will get off my back about it.

--- CS2340OregonTrail/src/CS2340OregonTrail/mordor/views.py
+++ CS2340OregonTrail/src/CS2340OregonTrail/mordor/views.py
@@ -2,19 +2,13 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
-from mordor.models import Party, Character, Store, Item, BaseItem, Wagon
+from mordor.models import Party, Character
 
 def start(request):
-    """
-    This function opens up the "create a new party page"
-    """
-    return render_to_response("mordor/styletest.html", {'next':"shop.php?id=0"},context_instance=RequestContext(request))
+    return render_to_response("mordor/start.html", {},context_instance=RequestContext(request))
     
 
 def submit(request):
-    """
-    Handler function for receiving sumbission from the new party screen. 
-    """
     try:
         if (request.POST['pp']):
             p = Party.objects.get(name=request.POST['pp'])
@@ -35,18 +29,7 @@ def submit(request):
         if request.POST[q]:
             m= Character(name=request.POST[q], profession = "", status = 1, health = 1, isLeader = False, party = partyz)
             m.save()
-    return HttpResponse("data received. visit <a href='../"+request.POST["next"]+"></a> to see your party.")
+    return HttpResponse("data received. visit <a href='../config.php'>config.php</a> to see your party.")
 
 def config(request):
-    """
-    This function will create a page that displays the currently available parties and their stats.
-    """
     return render_to_response("mordor/config.html", {'parties':Party.objects.all(), "membs":map(lambda a: a.character_set.all()[0].name[0], Party.objects.all())},context_instance=RequestContext(request))
-
-def shop(request):
-    idz = request.GET['id']
-    shop = Shop.objects.get(id=idz)
-    items = shop.item_set.all()
-    return render_to_response("mordor/shop.html", {'shop':shop, 'items':items},context_instance=RequestContext(request))
-    
-    
