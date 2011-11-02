88713b3a35ddd14e7c593fcf22ecb97ce568a41a
theDekel <thedekel@gmail.com>
2011-11-01 23:42:11 -0400

    oeunthoearuh,r.u

--- settings.py
+++ settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/thedekel/School/2340/mordor/stat',
+    '/home/thedekel/School/2340/sons-of-a-bit-gatech-2340/res/stat/',
 )
 
 # List of finder classes that know how to find static files in
