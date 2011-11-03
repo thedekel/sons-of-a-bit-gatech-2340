e0d010a93b32b19d990a4be024c10bdd3e75a276
theDekel <thedekel@gmail.com>
2011-11-03 04:16:51 -0400

    models and functions updated to work properly

--- mordor/functions.py
+++ mordor/functions.py
@@ -9,7 +9,9 @@ def takeATurn(partyid):
     if not party.consumeFood():
         stuck(partyid, "Your party is out of food and cannot proceed")
         return
-    moveLocation(partyid)
+    if moveLocation(partyid)=="event":
+        party.save()
+        #do something to indicate an event
     party.save()
 
 def stuck(partyid,msg):
@@ -82,70 +84,48 @@ def moveLocation(partyid): #int
             evtype = ev.etype
             aparty.stopmsg = ev.text()
             aparty.save()
-
-
-
-def populateLocations():
-    """
-    This generates the map, which sits in the database, referenced by an index element
-    Used once for instantiation
-    @return: A list of Locations
-    """
-    locations=[]
-    events = []
-    for num in range(131):
-        coord = get_player_coords(num)
-        loc = Location(x=coord[0],y=coord[1],index=num)
-        loc.save()
-        locations.append(loc)
-        eee=Event(location = loc)
-        eee.save()
-        events.append(eee)
-        
-    for z in [6, 39, 75, 125]:
-        locations[z] = Location(halt=True, index=z, x=locations[z].x, y=locations[z].y)
-        rce= RiverCrossingEvent(location = locations[z])
-        rce.save()
-        events[z]=rce
-    for a in [[0,"Hobbiton"],[16,"Bree"],[38,"Thrabad"],[74,"Gap of Rohan"],[89,"Edoras"],[106,"Minas Tirith"]]:
-        locations[a[0]] = Location(name=a[1],halt=True, index=a[0], x=locations[a[0]].x, y=locations[a[0]].y)
-        ste= StoreEvent(location = locations[a[0]],name = a[1])    
-        events[a[0]]=ste 
-    ll = Location(name = "Mordor",halt=True, index=130, x=locations[130].x, y=locations[130].y)
-    ll.save()
-    locations[130] = ll
-    events[130] = EndGame(location=locations[130])
+            return "event"
+    return
 
 def takeFerry(partyid): #string
     """
     Takes the Ferry. Takes a designated amount of money away from the player
     to use the ferry
     """
-    party = Party.objects.get(id=partyid)
+    aparty = Party.objects.get(id=partyid)
     party.money -= 250
+    party.stopmsg = "You paid 250 gold to cross the bridge"
     party.save()
-    return "you paid 250 currency to cross the bridge"
+    return
     
 def ford():
     """
     Attempts to ford the river. Fails if water depth is greater than 3 feet.
     @return: String: The success or failure of fording the river
     """ 
+    aparty = Party.objects.get(id=partyid)
     chance = random.randint(0,100)
     percentChance = 5 * 2
     if (chance > (100 - percentChance)):
-        msg = "DUDE FLIP THE WAGON!"
-        return msg
-    return 0
-    
+        aparty.stopmsg = "You failed to ford the river!"
+        aparty.save()
+        return
+    aparty.stopmsg = "You successfully forded the river!"
+    aparty.save()
+    return 
+
 def caulk():
     """
     Attempts to float over the river on the wagon. Greater chance of wagon flip.
     @return: String: The success or failure of caulking the river
     """
+    aparty = Party.objects.get(id=partyid)
     chance = random.randint(0,100)
     if (chance > 70):
-        msg = "DUDE FLIP THE WAGON"
-        return msg
-    return 0
+        aparty.stopmsg = "You couldn't float over the river"
+        aparty.stop()
+        return 
+    aparty.stopmsg = "You managed to float over the river"
+    aparty.stop()
+    return 
 
